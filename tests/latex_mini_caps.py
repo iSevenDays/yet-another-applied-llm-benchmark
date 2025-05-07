b5e8b7c93ac0210c9b9fba26de0c821894583b09
Anton Sokolchenko <wsevendays@gmail.com>
2025-05-07 09:07:25 +0200

    Fix typo in one unit test

--- tests/latex_mini_caps.py
+++ tests/latex_mini_caps.py
@@ -6,9 +6,9 @@ TAGS = ['explain']
 
 question = "Latex format the word METHOD like Method but all caps with the other letters lower"
 
-TextLatexLowerCaps = question >> LLMRun() >> SubstringEvaluator("textsc")
+TestLatexLowerCaps = question >> LLMRun() >> SubstringEvaluator("textsc")
 
 if __name__ == "__main__":
-    print(run_test(TextLatexLowerCaps))
+    print(run_test(TestLatexLowerCaps))
 
 
